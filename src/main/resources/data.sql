3832dc864d042a1cabb5f53801bea9bc8957a81f
yvivasUnosquare <161874973+yvivasUnosquare@users.noreply.github.com>
2024-06-08 10:33:38 -0500

    Update data.sql
    better image

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO supportportal.employee (id,email,employee_code,image_url,job_title,name,phone) VALUES
-    (0,'hulk@gmail.com','6705b8ed-3b44-4486-9d0e-7927759309f6','https://picsum.photos/111/333','HR','Bruce Banner','+18642337111');
+    (0,'hulk@gmail.com','6705b8ed-3b44-4486-9d0e-7927759309f6','https://bootdey.com/img/Content/avatar/avatar7.png','HR','Bruce Banner','+18642337111');
